38ba1d6549ec440747460d87ed84a6b4e8e481cb
Your Name <you@example.com>
2021-09-23 17:14:29 +0900

    블로그 비밀번호 해쉬 설정

--- src/main/java/com/cos/blogapp2/web/UserController.java
+++ src/main/java/com/cos/blogapp2/web/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.cos.blogapp2.domain.user.User;
 import com.cos.blogapp2.domain.user.UserRepository;
+import com.cos.blogapp2.util.SHA;
 import com.cos.blogapp2.web.dto.JoinReqDto;
 
 import lombok.RequiredArgsConstructor;
@@ -19,7 +20,12 @@ public class UserController {
 	
 	//  회원가입을 하면 로그인 화면으로 이동한다
 	@PostMapping("/join")
-	public String join(JoinReqDto dto) {
+	public String join(JoinReqDto dto) { 
+		// 데이터를 username=ssar&password=1234&email=ssar@nate.com 
+        // 마임타입 = urlencoded 형식으로 가져온다
+
+		String encPassword = SHA.encrypt(dto.getPassword());
+		dto.setPassword(encPassword);
 		
 		User user = dto.toEntity();
 		userRepository.save(user);
