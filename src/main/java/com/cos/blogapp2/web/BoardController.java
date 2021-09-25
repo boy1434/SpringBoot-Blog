24f69bce868451be13efb33d975e4008daa9f3ab
tkdwls1434 <boy1434@naver.com>
2021-09-25 18:57:47 +0900

    글쓰기 DB저장이 안됌

--- src/main/java/com/cos/blogapp2/web/BoardController.java
+++ src/main/java/com/cos/blogapp2/web/BoardController.java
@@ -3,10 +3,31 @@ package com.cos.blogapp2.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.cos.blogapp2.domain.board.Board;
+import com.cos.blogapp2.domain.board.BoardRepository;
+import com.cos.blogapp2.web.dto.BoardSaveReqDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller 
 public class BoardController {
+	
+	// final 이 붙으면 초기화를 해줘야하는데  초기화를 @RequiredArgsConstructor로 한다
+	// 하면 private final BoardRepository boardRepository; 에대한 생성자가 생김 
+	private final BoardRepository boardRepository;
 
+	@PostMapping("/board")
+	public String save(BoardSaveReqDto dto) {
+		
+		Board board = dto.toEntity();
+		boardRepository.save(board);
+		return "redirect:/";
+	}
+	
+		
 	// 글 전체 목록으로 이동
 	@GetMapping("/board")
 	public String list() {
